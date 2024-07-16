746e0e89bd1dd639daf7d28b7b3b661ee2ccad06
otcap <gertjanblom@gmail.com>
2024-07-16 23:26:38 +0200

    Create rekenen.java

---
+++ java/quickstart/code/rekenen/rekenen.java
@@ -0,0 +1,7 @@
+package com.capgemini.training;
+public class rekenen {
+public static void main(String[] args) {
+//String[] mijngetallen = { "1", "2", "3" };
+//args = mijngetallen ;
+int som =bereken.optellen(args);
+System.out.println(som); }}
